a09ae6f6c770b63f6e7395cf67c69a8ed22a1cb1
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-03-28 23:42:19 -0700

    Adding Project2_functions

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {    //naming the function
+    var sentence = "AOLCC";  //assigning variables
+   sentence += " is the best!"; //concatenating
+    document.getElementById("Concatenate").innerHTML = sentence; 
+}

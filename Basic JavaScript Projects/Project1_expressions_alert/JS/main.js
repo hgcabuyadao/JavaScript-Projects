b1e5b41bb5a80d890cf1fcfee8742159426bc3fc
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-03-28 22:54:52 -0700

    Create folder Project1_expressions_alert

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+window.alert("Welcome!"); //assigning string to windows alert
+var words= "Assigment 1", First="Assigment ", Second="Submission #1"; //Assigning 2 variable in an statement 
+var display=(First + Second);                               //Assigning 2 variable in an statement and concatenating them
+var display = display.fontcolor("red");                 //Assignin color to the selected variables
+document.write(display);                            //displaying variables
+document.write("<br>");                             //new line
+document.write("Let's add 3 and 5")                 //displaying string
+document.write("<br>");                     //new line
+document.write("The sum is:")                    //displaying string
+document.write("\n");                       //space
+document.write(3+5);                        //displaying sum of variables
+
